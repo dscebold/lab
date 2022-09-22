717f528976fda1f64e9fe56241f25c0bfb5687d0
dscebold <114021633+dscebold@users.noreply.github.com>
2022-09-22 10:59:40 -0500

    created three methods

--- account.py
+++ account.py
@@ -11,3 +11,15 @@ class Account:
             return False
         self.__account_balance += amount
         return True
+
+    def withdraw(self, amount):
+        if amount < 0 or amount > self.__account_balance:
+            return False
+        self.__account_balance -= amount
+        return True
+
+    def get_balance(self):
+        return self.__account_balance
+
+    def get_name(self):
+        return self.__account_name
