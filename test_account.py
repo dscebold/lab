bdde91f494f24dad5263baf01563fe6c4c58c508
dscebold <114021633+dscebold@users.noreply.github.com>
2022-11-15 20:52:06 -0600

    updates
    changed test_account.py from unnitetest to pytest

--- test_account.py
+++ test_account.py
@@ -1,39 +1,38 @@
-import unittest
+# import unittest
+import pytest
 from account import *
 
 
-class TestFormula(unittest.TestCase):
-
-    def setUp(self):
+class Test:
+    def setup_method(self):
         self.acc1 = Account("john")
         self.acc2 = Account('jane')
         self.acc3 = Account(4)
 
     def test_init(self):
-        self.assertEqual(self.acc1.get_name(), "john")
-        self.assertEqual(self.acc2.get_name(), "jane")
-        self.assertEqual(self.acc3.get_name(), 4)
-        self.assertEqual(self.acc1.get_balance(), 0)
-        self.assertEqual(self.acc2.get_balance(), 0)
-        self.assertEqual(self.acc3.get_balance(), 0)
+        assert self.acc1.get_name() == "john"
+        assert self.acc2.get_name() == "jane"
+        assert self.acc3.get_name() == 4
+        assert self.acc1.get_balance() == 0
+        assert self.acc2.get_balance() == 0
+        assert self.acc3.get_balance() == 0
         self.acc1.deposit(10)
         self.acc2.deposit(14)
         self.acc3.deposit(5)
-        self.assertEqual(self.acc1.get_balance(), 10)
-        self.assertEqual(self.acc2.get_balance(), 14)
-        self.assertEqual(self.acc3.get_balance(), 5)
+        assert self.acc1.get_balance() == 10
+        assert self.acc2.get_balance() == 14
+        assert self.acc3.get_balance() == 5
         self.acc1.withdraw(5)
         self.acc2.withdraw(4)
         self.acc3.withdraw(3)
-        self.assertEqual(self.acc1.get_balance(), 5)
-        self.assertEqual(self.acc2.get_balance(), 10)
-        self.assertEqual(self.acc3.get_balance(), 2)
-
+        assert self.acc1.get_balance() == 5
+        assert self.acc2.get_balance() == 10
+        assert self.acc3.get_balance() == 2
 
     def test_deposit(self):
-        self.assertEqual(self.acc1.deposit(4), True)
-        self.assertEqual(self.acc1.deposit(-4), False)
-        with self.assertRaises(ValueError):
+        assert self.acc1.deposit(4) == True
+        assert self.acc1.deposit(-4) == False
+        with pytest.raises(ValueError):
             self.acc1.deposit("hhh")
             self.acc2.deposit("ten")
             self.acc3.deposit("jane")
@@ -42,18 +41,16 @@ class TestFormula(unittest.TestCase):
         self.acc1.deposit(10)
         self.acc2.deposit(10)
         self.acc3.deposit(10)
-        self.assertEqual(self.acc1.withdraw(4), True)
-        self.assertEqual(self.acc1.withdraw(-4), False)
-        self.assertEqual(self.acc2.withdraw(10), True)
-        self.assertEqual(self.acc2.withdraw(-4), False)
-        self.assertEqual(self.acc3.withdraw(11), False)
-        self.assertEqual(self.acc3.withdraw(-4), False)
-        with self.assertRaises(ValueError):
+        assert self.acc1.withdraw(4) == True
+        assert self.acc1.withdraw(-4) == False
+        assert self.acc2.withdraw(10) == True
+        assert self.acc2.withdraw(-4) == False
+        assert self.acc3.withdraw(11) == False
+        assert self.acc3.withdraw(-4) == False
+        with pytest.raises(ValueError):
             self.acc1.withdraw("hhh")
             self.acc2.withdraw("ten")
             self.acc3.withdraw("jane")
 
-
-
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
